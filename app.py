9f39ebe407f8e51a5627307e9ec008c63ce04b69
juan <jadevlaplata@gmail.com>
2022-09-23 08:34:26 -0300

    otro cambio

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     args=request.args
     param1=args["name"] 
-    return "Hola,ESTO ES UNA PRUEBA PARA DSSD DE: "+ param1
+    return "Hola2,ESTO ES UNA PRUEBA PARA DSSD DE: "+ param1
 
 port = int(os.environ.get("PORT", 8000)) 
 app.run(host='0.0.0.0', port=port) 
